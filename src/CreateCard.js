0ec110064dac76166fc50cb2e6811bb255c81b6b
Atalia Mucharsky <atalia.mucharsky@hotmail.com>
2020-09-21 17:05:13 +0300

    fixes

--- src/CreateCard.js
+++ src/CreateCard.js
@@ -6,17 +6,17 @@ export default class CreateCard extends React.Component {
     }
 
     handleImageChange = event => {
-        this.setState({image: event.target.value });
+        this.setState({ image: event.target.value });
     }
 
     handleNameChange = event => {
-        this.setState({ name: event.target.value});
+        this.setState({ name: event.target.value });
     }
     handleJobChange = event => {
-        this.setState({ job: event.target.value});
+        this.setState({ job: event.target.value });
     }
     handleAgeChange = event => {
-        this.setState({ age: event.target.value});
+        this.setState({ age: event.target.value });
     }
 
     handleSubmit = async event => {
@@ -24,23 +24,21 @@ export default class CreateCard extends React.Component {
         document.form.reset();
         const warning = document.querySelector("h5");
         warning.style.display = "none";
-        const imageVerifier = new RegExp ("(http(s?):)|([/|.|w|s])*.(?:jpg|gif|png)");
+        const imageVerifier = new RegExp("(http(s?):)|([/|.|w|s])*.(?:jpg|gif|png)");
         const imageVerifierRes = imageVerifier.test(this.state.image);
         console.log(imageVerifierRes);
 
         if (this.state.name.length >= 5 && imageVerifierRes) {
 
             this.props.createCard(this.state.name, this.state.image, this.state.age, this.state.job);
-            this.setState({job: ''});
-            this.setState({age: ''});
-            
-            
+            this.setState({ job: '' });
+            this.setState({ age: '' });
         }
         else {
             warning.style.display = "block";
         }
-        this.setState({name : ''}); //so the user won't submit the same avatar multiple times
-        this.setState({image: ''});
+        this.setState({ name: '' }); //so the user won't submit the same avatar multiple times
+        this.setState({ image: '' });
 
     }
 
@@ -48,24 +46,24 @@ export default class CreateCard extends React.Component {
         return (
             <div>
                 <form name="form" onSubmit={this.handleSubmit}>
-                    <label style={{margin:"0 5px"}}>
+                    <label style={{ margin: "0 5px" }}>
                         Name:
-              <input style={{margin:"0 5px"}} type="text" name="name" onChange={this.handleNameChange} />
+              <input style={{ margin: "0 5px" }} type="text" name="name" onChange={this.handleNameChange} />
                     </label>
-                    <label style={{margin:"0 5px"}}>
+                    <label style={{ margin: "0 5px" }}>
                         Age:
-              <input style={{margin:"0 5px"}} type="text" age="age" onChange={this.handleAgeChange} />
+              <input style={{ margin: "0 5px" }} type="text" age="age" onChange={this.handleAgeChange} />
                     </label>
-                    <label style={{margin:"0 5px"}}>
+                    <label style={{ margin: "0 5px" }}>
                         Job:
-              <input style={{margin:"0 5px"}} type="text" job="job" onChange={this.handleJobChange} />
+              <input style={{ margin: "0 5px" }} type="text" job="job" onChange={this.handleJobChange} />
                     </label>
-                    <label style={{margin:"0 5px"}}>
-              Image URL:
-              <input style={{margin:"0 5px"}} type="text" image="image" onChange={this.handleImageChange} />
-            </label>
+                    <label style={{ margin: "0 5px" }}>
+                        Image URL:
+              <input style={{ margin: "0 5px" }} type="text" image="image" onChange={this.handleImageChange} />
+                    </label>
                     <button type="submit">Add Avatar</button>
-                    <h5 style={{ display: "none" }}>Please check that you enter name with minimum 5 letters and valid image url</h5>
+                    <h5 style={{ display: "none" }}>Please check that you enter a name with minimum 5 letters and valid image url</h5>
                 </form>
             </div>
         )
